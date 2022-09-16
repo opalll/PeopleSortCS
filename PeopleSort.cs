3d40cfb1ee22d5da1e4cf50999cc79a6938e564f
opalll <whitestoneabc123@gmail.com>
2022-09-17 03:13:36 +0800

    add PeopleSort.cs and Program.cs

---
+++ PeopleSort.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testcs
+{
+    class PeopleSort
+    {
+        public string name;
+        public int age;
+        public int number;
+        private static PeopleSort[] box = new PeopleSort[10000];
+        private string status;
+        public static int count;
+        public PeopleSort(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+            Status = status;
+            count++;
+            this.number = count;
+        }
+        public string Status
+        {
+            get
+            {
+                return status;
+            }
+            set
+            {
+                if (age >= 0 && age < 18)
+                {
+                    status = "young";
+                }
+                else if (age >= 18 && age < 64)
+                {
+                    status = "adult";
+                }
+                else if (age >= 64)
+                {
+                    status = "old";
+                }
+                else
+                {
+                    status = "error";
+                }
+            }
+        }
+        public static void CheckIn(PeopleSort ppl)
+        {
+            box[count] = ppl;
+        }
+        public static void ShowPeople(int num)
+        {
+            Console.WriteLine(box[num].name);
+            Console.WriteLine(box[num].age);
+            Console.WriteLine(box[num].Status);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using testcs;
+
+Console.WriteLine("Enter \"END\" if you want to stop");
+while (true)
+{
+    string name = Console.ReadLine();
+    if (name == "END")
+        break;
+    int age = Convert.ToInt32(Console.ReadLine());
+    PeopleSort people = new PeopleSort(name, age);
+    PeopleSort.CheckIn(people);
+}
+
+while(true)
+{
+    int num = Convert.ToInt32(Console.ReadLine());
+    PeopleSort.ShowPeople(num);
+}
